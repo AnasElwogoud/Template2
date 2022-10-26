0b33e17e493e0780816ea60ed0dc21b255442843
Anas Elwogoud Elsayed <108733036+AnasElwogoud@users.noreply.github.com>
2022-10-27 01:24:54 +0200

    Template Two

---
+++ js/script.js
@@ -0,0 +1,621 @@
+// var salary = prompt("what's ur salary")
+
+// console.log(salary = 12 + salary)
+
+// var arr = [0, 10, 20, 30];
+// arr[4] = 40;
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+
+// var person = new Object();
+// person.username = 'anas';
+// person.address = 'Giza';
+// console.log(person);
+
+// delete person.address;
+
+// console.log(person);
+
+// var x = 'hello ';
+
+// var y = x;
+
+// x = "test";
+
+// console.log(x);
+// console.log(y + x);
+
+// var person = {
+//     username: 'anas',
+//     address: 'Cairo',
+// };
+
+// var data = person;
+// data.username = "ahmed";
+// person.address = "Giza";
+
+// console.log(data);
+// console.log(person);
+
+// function print_error() {
+//     console.log("report de7k");
+//     console.log("report de7kez");
+// }
+
+// print_error();
+
+// function print_report(reporty_type) {
+//     console.log('report at ' + reporty_type)
+// }
+
+// print_report('day');
+// print_report('month');
+// print_report('year');
+
+// var salary = Number(prompt("what's ur salary"));
+// var salary2 = Number(prompt("what's ur 2nd salary"));
+
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// function minous(x, y) {
+//     console.log(x - y);
+// }
+// function multiplay(x, y) {
+//     console.log(x * y);
+// }
+// function divided(x, y) {
+//     console.log(x / y);
+// }
+// function percent(x, y) {
+//     console.log(x % y);
+// }
+
+// sum(salary, salary2);
+// minous(salary, salary2);
+// multiplay(salary, salary2);
+// divided(salary, salary2);
+// percent(salary, salary2);
+
+// var salary = Number(prompt("what's ur salary"));
+// var salary2 = Number(prompt("what's ur 2nd salary"));
+
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// sum(salary, salary2);
+
+// function annual_salary(x = 1) {
+//     return x * 12;
+// }
+
+// var total_salary = annual_salary() + 125 - 500;
+// console.log(total_salary);
+
+// function annual_salary(x = 1, y = 1) {
+//     return Object({ x, y });
+// }
+
+// var total_salary = annual_salary(10, 20);
+// console.log(total_salary);
+
+// function total(x, y) {
+//     return x + y;
+// }
+// console.log(total(5, 6));
+
+// var total = function (x, y) {
+//     return x + y;
+// }
+
+// console.log(total(5, 6))
+
+// function total(x, y) {
+//     return [x + y,
+//     x * y,
+//     x - y,
+//     x % y,
+//     x / y];
+// }
+// console.log(total(5, 6));
+
+// function report(x) {
+//     x = 'test';
+//     console.log(x);
+// }
+
+// console.log(x);
+// report(x);
+// console.log(x);
+
+// 'use strict'
+// function test() {
+//     x = 10;
+//     console.log(x);
+// }
+
+// test();
+
+// console.log(x);
+
+// var x = 10;
+// x += 5
+// x -= 5
+// console.log(x);
+
+// var x = 10;
+// x += 5
+// x -= 5
+// x *= 5
+// x /= 5
+// x %= 5
+// console.log(x);
+
+// var x = 10;
+// var y = --x;
+// console.log(x);
+// console.log(y);
+
+// var x = 10;
+// var y = x--;
+// console.log(y);
+// console.log(x);
+
+// var x = 5;
+// if (x <= 6) {
+//     console.log('YES');
+// }
+// else {
+//     console.log("NO")
+// }
+
+// var percentage = Number(prompt('Please, Enter ur percentage grade'))
+
+// if (percentage >= 85) {
+//     console.log('Excellent');
+// }
+// if (percentage >= 75) {
+//     console.log('V.Good');
+// }
+// if (percentage >= 65) {
+//     console.log('Good');
+// }
+// else if (percentage >= 50) {
+//     console.log('Passed');
+// }
+// else {
+//     console.log("YA FASHEL YA FASHEL")
+// }
+
+// var salary = Number(prompt('Please, Enter ur percentage grade'))
+
+// if (salary <= 1000) {
+//     console.log('Trainer');
+// }
+// else if (salary <= 2000) {
+//     console.log('Junior');
+// }
+// else if (salary <= 3000) {
+//     console.log('Senior');
+// }
+// else if (salary <= 4000) {
+//     console.log('Team Lead');
+// }
+// else if (salary >= 4000) {
+//     console.log('HR');
+// }
+
+// var n = Number(prompt('Check number'))
+
+// if (n % 2 == 0) {
+//     console.log('Number is EVEN')
+// }
+// else {
+//     console.log('Number is ODD')
+// }
+
+// var x = 10;
+
+// switch (x) {
+//     case 10: console.log('its 10');
+//         break;
+//     case 2: console.log('its 2');
+//         break;
+//     case 30: console.log('its 30');
+//         break;
+//     default: console.log('errors')
+// }
+
+// var x = 100;
+// var y = 20;
+
+// switch (true) {
+//     case (x > 2 && y < 5): console.log('its 2');
+//         break;
+//     case (x == 10): console.log('its 10');
+//         break;
+//     case (x <= 100): console.log('its 30');
+//         break;
+//     default: console.log('errors')
+// }
+
+// var x = 10;
+// var y = (x > 20) ? "yes" : "no";
+
+// console.log(y);
+
+// var x = prompt('Enter number')
+
+// if (isNaN(x)) {
+//     console.log('Error');
+// } else {
+//     console.log(Number(x) + 50);
+// }
+
+// arr = ['ali', 'saad', 'mohamed', 'hassan']
+// for (let i = 0; i < 4; i++) {
+//     console.log(arr[i]);
+// }
+
+// var num = Number(prompt('Please Enter number of products'));
+// var total = 0;
+
+// for (let i = 0; i < num; i++) {
+//     total += Number(prompt('enter price of each one'));
+// }
+
+// console.log(total);
+
+// for (let i = 0; i < 4; i++) {
+//     if (i == 2) {
+//         break;
+//     }
+//     console.log(i);
+// }
+// console.log('NO');
+
+// for (let i = 0; i < 4; i++) {
+//     if (i == 2 || i == 1) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// var arr = ['ahmed', 'sehs', 'gedo', 'sasa']
+// var counter = 100;
+// for (let item in arr) {
+//     console.log(arr[item]);
+// }
+// console.log('--------------------------')
+
+// for (let item of arr) {
+//     console.log(item);
+// }
+// console.log('--------------------------')
+
+// for (let i = 0; i < 4; i++) {
+//     console.log(arr[i]);
+// }
+// console.log('--------------------------')
+
+// console.log('--------------------------')
+
+// do {
+//     console.log(counter);
+//     console.log(arr[counter]);
+//     counter++;
+// } while (counter < arr.length);
+
+// var person = {
+//     id: 10,
+//     name: 'Ali',
+//     address: 'Cairo'
+// };
+
+// for (let key in person) {
+//     console.log(person[key]);
+// }
+
+// var num = Number(prompt('Please Enter number of students'));
+
+// for (let i = 0; i < num; i++) {
+//     x = prompt('enter name of each one');
+//     console.log(x);
+// }
+
+// var salary = prompt('enter salary');
+// while (isNaN(salary)) {
+//     salary = prompt('enter valid number');
+// }
+// console.log('annual salary = ' + (salary * 12))
+
+// num_products = prompt(Number('enter a number of products'));
+// max_value = 150;
+// for (let max_value = 0; i < max_value; i++) {
+//     total += Number(prompt('enter price of each one'));
+// }
+
+// console.log(total);
+
+// function emp(a, b, c) {
+//     this.salary = a;
+//     this.address = b;
+//     this.skills = c;
+// }
+
+// var data = new emp(100, 'cairo', 'web')
+// console.log(data)
+
+// var user = {
+//     username: 'ahmed',
+//     address: 'cairo',
+// };
+// var person = Object.assign({}, user);
+// user.address = 'giza';
+// console.log(user);
+// console.log(person);
+
+// var username = "anas-asdas";
+// console.log(username.length)
+// console.log(username.toUpperCase())
+// console.log(username.toLowerCase())
+// console.log(username.replace('a', 'g'))
+// console.log(username.replaceAll('a', 'g'))
+// console.log(username.concat(' elwogoud', ' testto', ' aba'))
+// console.log(username.search("anas")); // == indexOf
+// console.log(username.trim());
+// console.log(username.split("-")); // change to array and remove (-)
+// console.log(username.charAt("0")); // no. of letter
+// console.log(username.slice(0, 5)); // (start index,end index )(-3 bygeb mn el a5r w y3d)
+// console.log(username.indexOf("a")); // first letter in sentencs
+// console.log(username.lastIndexOf("a")); // last letter in sentencs
+// console.log(username[0])
+// console.log(username[1])
+// console.log(username[2])
+// console.log(username[3])
+
+// var email = prompt("Enter Email");
+// while (
+//   email.indexOf("@") == -1 ||
+//   email.indexOf(".com") == -1 ||
+//   email.indexOf("gmail") == -1
+// ) {
+//   email = prompt("Enter Valid Email");
+// }
+// console.log("your email is: " + email);
+
+// var price = 140.687775548;
+// console.log(price.toFixed()); // no decimal
+// console.log(price.toFixed(1)); // 1st decimal
+// console.log(price.toFixed(2));
+// console.log(price.toFixed(3));
+
+// var arr = ["anas", "mazen", "marvel", "sehs"];
+
+// arr.push("test"); // add new item at last of array
+// arr.unshift("test"); // add new item at start of array
+// arr.pop(); // remove last item from array
+// arr.shift(); // remove first item from array
+// console.log(arr);
+// console.log(arr.reverse()); // tarteb bel 4a2lob
+// console.log(arr.sort()); // tarteb abgdy A B C D
+// console.log(arr.join("||**")); // add || & ** between names
+
+// function sort_arr(val, index) {
+//   console.log(val);
+//   console.log(index);
+// }
+// arr.forEach(sort_arr);
+
+// var date = new Date();
+
+// console.log(date);
+
+// console.log(date.getDay());
+// console.log(date.getDate());
+// console.log(date.getMonth() + 1); // lazm +1 3a4an bybd2 mn 0
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getMilliseconds());
+
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toDateString());
+// console.log(date.toString());
+
+// var x = 10.1;
+
+// console.log(Math.round(x));
+// console.log(Math.ceil(x));
+// console.log(Math.floor(x)); // take integer number
+
+// ----------------------DOM---------------
+
+// var first_p = document.getElementById("one");
+// var all_p = document.getElementsByTagName("p");
+// var test_class = document.getElementsByClassName("test");
+// var parent_span = document.querySelector(".parent .son .test span");
+// var all_span = document.querySelectorAll(".parent .son .test span");
+
+// console.log(first_p);
+// console.log(all_p[2]);
+// console.log(test_class);
+// console.log(parent_span);
+// console.log(all_span);
+
+// var first_p = document.querySelector("p");
+// first_p.innerHTML = "<span>Change HTML</span>";
+
+// var num = Number(prompt("Enter a positive number:"));
+// let x = true;
+
+// if (isNaN(num)) {
+//   console.log("error");
+// }
+// if (num >= 1) {
+//   for (let i = 2; i < num; i++) {
+//     if (num % i == 0) {
+//       x = false;
+//       break;
+//     }
+//   }
+//   if (x) {
+//     console.log(num + " is a prime number");
+//   } else {
+//     console.log(num + " is a not prime number");
+//   }
+// }
+
+// var num = Number(prompt("enter ur number"));
+// var status = "prime";
+// for (let i = 2; i < num; i++) {
+//   if (num % i == 0) {
+//     status = "not prime";
+//     break;
+//   }
+// }
+// console.log(status);
+
+// document.querySelector("input").onkeyup = function () {
+//   let num = document.querySelector("input").value.length;
+//   var p = document.querySelector("p");
+//   if (num <= 5) {
+//     p.textContent = "weak password";
+//   } else if (num <= 9) {
+//     p.textContent = "medium password";
+//   } else {
+//     p.textContent = "Strong password";
+//   }
+// };
+
+// a5tsar lel ale fo2 */
+// document.querySelector("input").onkeyup = function () {
+//   let num = event.target.value.length;
+//   event.target.nextElementSibling.style.paddingLeft = "10px";
+//   //   event.target.nextElementSibling.style.backgroundColor = "black";
+//   document.querySelector("p").style.cssText =
+//     "color:black;font-size:50px;margin:30px;";
+//   if (num <= 5) {
+//     event.target.nextElementSibling.textContent = "weak password";
+//     event.target.nextElementSibling.style.color = "red";
+//     event.target.nextElementSibling.className = "alert alert-danger";
+//   } else if (num <= 9) {
+//     event.target.nextElementSibling.textContent = "medium password";
+//     event.target.nextElementSibling.style.color = "orange";
+//     event.target.nextElementSibling.className = "alert alert-warning";
+//   } else {
+//     event.target.nextElementSibling.textContent = "Strong password";
+//     event.target.nextElementSibling.style.color = "green";
+//     event.target.nextElementSibling.className = "alert alert-primary";
+//   }
+// };
+
+// document.querySelector("input").onclick = function () {
+//   console.log(event.target);
+//   console.log(event.target.nextElementSibling);
+//   console.log(event.target.previousElementSibling);
+//   console.log(event.target.parentElement.parentElement);
+// };
+
+// document.querySelector("input").onclick = function () {
+//   event.target.classList.toggle("test");
+// };
+// document.querySelector("p").classList.add("test");
+// document.querySelector("p").classList.remove("test");
+// document.querySelector("p").classList.toggle("test");
+
+// console.log(document.querySelector("p").classList);
+
+// document.querySelector("button").onclick = function () {
+//   if (event.target.previousElementSibling.value.length == 0) {
+//     alert("please enter your skill first");
+//   } else {
+//     var li = document.createElement("li");
+//     li.textContent = event.target.previousElementSibling.value;
+//     event.target.nextElementSibling.append(li);
+//     event.target.previousElementSibling.value = "";
+//   }
+// };
+
+// document.querySelector("form").onsubmit = function () {
+//   event.preventDefault();
+//   let tb = document.querySelector("tbody");
+//   let tr = document.querySelector("tr");
+//   tb.append(tr);
+//   let td = document.createElement("td");
+//   tr.append(td);
+//   td.textContent = document.querySelector("[name='name']").value;
+//   let td2 = document.createElement("td");
+//   tr.append(td2);
+//   tb2.textContent = document.querySelector("[name='address']").value;
+//   let td3 = document.createElement("td");
+//   tr.append(td3);
+//   tb3.textContent = document.querySelector("[name='skills']").value;
+// };
+
+// var tr = document.createElement("tr");
+// tr.innerHTML = "<td>ahmed</td><td>ahmed</td><td>ahmed</td>";
+// document.querySelector("table tbody").append(tr);
+
+// for (let color of document.querySelectorAll(".color_styles ul li")) {
+//   color.onclick = function (e) {
+//     document
+//       .querySelector("head link:last-of-type")
+//       .setAttribute(
+//         "href",
+//         "css/color/" + e.target.getAttribute("name") + ".css"
+//       );
+//   };
+// }
+
+// for (let ad_input of document.querySelectorAll(".ad form input:not(.btn)")) {
+//   ad_input.onkeyup = function () {
+//     var input_val = event.target.value;
+//     document.querySelector(
+//       '.preview-ad p[show="' + event.target.getAttribute("placeholder") + '"]'
+//     ).textContent = input_val;
+//   };
+// }
+
+// var data = {
+//   egy: ["cairo", "alex", "zagzig", "banha"],
+//   usa: ["NY", "Chicago", "Washinton"],
+//   ksa: ["Madena", "Reyad", "Mka"],
+// };
+// document.querySelector(
+//   ".country_cities form div:first-of-type select"
+// ).onchange = function () {
+//   let output = '<option "">Select city</option>';
+//   if (event.target.value != "") {
+//     var cities = data[event.target.value];
+//     for (let city of cities) {
+//       output += '<option value="' + city + '">' + city + "</option>";
+//     }
+//   }
+//   document.querySelector(
+//     ".country_cities form div:last-of-type select"
+//   ).innerHTML = output;
+// };
+
+// Close shape 1
+// document.querySelector("h5 span:last-of-type").onclick = function () {
+//   if (confirm("are you sure from delete")) {
+//     event.target.parentElement.remove();
+//   }
+// };
+
+// Close Shape 2
+// document
+//   .querySelector("h5 span:last-of-type")
+//   .addEventListener("click", function () {
+//     if (confirm("are you sure from delete")) {
+//       event.target.parentElement.remove();
+//     }
+//   });
---
+++ js/script2.js
@@ -0,0 +1,84 @@
+// var data = [
+//     {
+//         id: 10,
+//         username: 'ali',
+//         skills: [
+//             {
+//                 html: '5 years',
+//                 projects: 5,
+//             }, {
+//                 css: '6 years',
+//                 projects: 10,
+//             }
+//         ]
+//     }, {
+//         id: 15,
+//         username: 'saad',
+//         skills: [
+//             {
+//                 php: '5 years',
+//                 projects: 5,
+//             }, {
+//                 mysql: '6 years',
+//                 projects: 10,
+//             }
+//         ]
+//     }
+// ]
+
+// console.log(data[1])
+
+// var arr = [0, 10, 20, 30];
+// arr[4] = 40;
+// arr[0] = 5
+// console.log(arr)
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+
+// var person = new Object();
+// person.username = 'anas';
+// person.address = 'Giza';
+// console.log(person);
+
+// person.username = 'ahmed'
+// delete person.address;
+
+// console.log(person);
+
+// var salary = Number(prompt("what's ur salary"));
+// var salary2 = Number(prompt("what's ur 2nd salary"));
+
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+// function minous(x, y) {
+//     console.log(x - y);
+// }
+// function multiplay(x, y) {
+//     console.log(x * y);
+// }
+// function divided(x, y) {
+//     console.log(x / y);
+// }
+// function percent(x, y) {
+//     console.log(x % y);
+// }
+
+// sum(salary, salary2);
+// minous(salary, salary2);
+// multiplay(salary, salary2);
+// divided(salary, salary2);
+// percent(salary, salary2);
+
+// var num = Number(prompt("enter ur number"));
+// var status = "prime";
+// for (let i = 2; i < num; i++) {
+//   if (num % i == 0) {
+//     status = "not prime";
+//     break;
+//   }
+// }
+
+// console.log(status);
